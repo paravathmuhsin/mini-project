092ff5e02dd2a110d5c0c997d8ada7749ff1eb5d
archanarkamath <rarchana.archana95@gmail.com>
2025-03-03 07:48:06 +0530

    new changes

--- src/pages/Todos/Details.jsx
+++ src/pages/Todos/Details.jsx
@@ -3,7 +3,6 @@ import { useParams } from "react-router";
 import { gettodo, getuser } from "../../services/post.service";
 import "../../assets/style.scss";
 
-
 const Details = () => {
   const { id } = useParams();
   const [todos, setTodos] = useState();
@@ -14,31 +13,32 @@ const Details = () => {
       setTodos(res);
       setUser(res.userId);
     });
-    getuser(id).then((res)=>{
-      setUser_details(res);
+    getuser(id).then((res1) => {
+      setUser_details(res1);
     });
-    console.log(user_detail);
-  }, [id,user]);
-  
+  }, [id, user]);
+
   return (
     <div>
       <h1>Details of todos</h1>
       {todos && (
-        
         <div className="card_detail_wrapper">
-          <div className={(todos.completed ? "c_green" : "nc_red") +" card_detail_header"}>{todos.completed ? "Completed" : "Not Completed"}</div>
-          <div className="card_detail_content">
-          <h2>Todos ID : {todos.id}</h2>
-          <h2>User ID : {todos.userId},
-          </h2>
-          <h2 className="">Todos title : {todos.title}</h2>
-          <div>
-            <h2>Owner of this todos : {user_detail.name}</h2>
-            
-            </div>
-
+          <div
+            className={
+              (todos.completed ? "c_green" : "nc_red") + " card_detail_header"
+            }
+          >
+            {todos.completed ? "Completed" : "Not Completed"}
           </div>
-          
+          <div className="card_detail_content">
+            <h2>Todos ID : {todos.id}</h2>
+            <h2>User ID : {todos.userId}</h2>
+            <h2 className="">Todos title : {todos.title}</h2>
+            <div>
+              <h2 className="user_detail">Owner details</h2>
+              <h3>Name: {user_detail.name}</h3>
+            </div>
+          </div>
         </div>
       )}
     </div>

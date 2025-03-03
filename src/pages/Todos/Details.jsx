a82154d4fa626f8b9d45df4a6926b6165dfe939d
Harishma <harishmap660@gmail.com>
2025-03-02 22:15:39 -0800

    adding userlist2

--- src/pages/Todos/Details.jsx
+++ src/pages/Todos/Details.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router";
-import { gettodo, getuser } from "../../services/post.service";
+import { gettodo, getUser } from "../../services/post.service"
 import "./style.scss";
 
 const Details = () => {
